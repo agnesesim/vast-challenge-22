56eccce016acbc0f9ce67a28337bb1c91b89e315
agnesesim <38429734+agnesesim@users.noreply.github.com>
2023-05-01 14:40:42 +0200

    dynamic home page

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,46 +1,67 @@
 <template>
-  <v-row>
-    <v-col class="d-flex justify-space-evenly">
-      <v-card flat height="200" width="200" class="d-flex flex-column justify-center align-center rounded-lg">
-        <v-img
-          src="src\assets\icons\profile.png"
-          contain
-          width="100"
-        ></v-img>
-        <div class="text-h6 pb-3">Abitanti</div>
-      </v-card>
-      <v-card flat height="200" width="200" class="d-flex flex-column justify-center align-center rounded-lg">
-        <v-img
-          src="src\assets\icons\store.png"
-          contain
-          width="100"
-        ></v-img>
-        <div class="text-h6 pb-3">Negozi</div>
-      </v-card>
-      <v-card flat height="200" width="200" class="d-flex flex-column justify-center align-center rounded-lg">
-        <v-img
-          src="src\assets\icons\network.png"
-          contain
-          width="100"
-        ></v-img>
-        <div class="text-h6 pb-3">Relazioni</div>
-      </v-card>
-      <v-card flat height="200" width="200" class="d-flex flex-column justify-center align-center rounded-lg">
-        <v-img
-          src="src\assets\icons\people.png"
-          contain
-          width="100"
-        ></v-img>
-        <div class="text-h6 pb-3">Demografia</div>
-      </v-card>
-      <v-card flat height="200" width="200" class="d-flex flex-column justify-center align-center rounded-lg">
-        <v-img
-          src="src\assets\icons\treasure-map.png"
-          contain
-          width="100"
-        ></v-img>
-        <div class="text-h6 pb-3">Mappa</div>
-      </v-card>
-    </v-col>
-  </v-row>
+    <v-row class="d-flex justify-space-evenly">
+      <template v-for="(item, i) in items" :key="i">
+        <v-col
+          cols="6"
+          md="2"
+        >
+          <v-hover v-slot="{ isHovering, props }">
+            <v-card
+              flat height="200" width="200"
+              class="d-flex flex-column justify-center align-center rounded-lg"
+              :elevation="isHovering ? 12 : 2"
+              :class="{ 'on-hover': isHovering }"
+              v-bind="props"
+              :to="'/'+ item.link"
+            >
+              <v-img
+                contain
+                width="100"
+                :src="item.img"
+              >
+              </v-img>
+              <div class="text-h6 pb-3">{{item.title}}</div>
+            </v-card>
+          </v-hover>
+        </v-col>
+      </template>
+    </v-row>
 </template>
+
+<script>
+ 
+export default {
+  data() {
+    return {
+      items: [
+        {
+          title: 'Abitanti',
+          link: `abitanti`,
+          img: 'src\\assets\\icons\\profile.png',
+        },
+        {
+          title: 'Negozi',
+          link: `AbitantiView`,
+          img: 'src\\assets\\icons\\store.png',
+        },
+        {
+          title: 'Relazioni',
+          link: `AbitantiView`,
+          img: 'src\\assets\\icons\\network.png',
+        },
+        {
+          title: 'Demografia',
+          link: `AbitantiView`,
+          img: 'src\\assets\\icons\\people.png',
+        },
+        {
+          title: 'Mappa',
+          link: `AbitantiView`,
+          img: 'src\\assets\\icons\\treasure-map.png',
+        },
+      ],
+    }
+  }
+}
+
+</script>
